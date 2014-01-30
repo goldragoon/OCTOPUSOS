0980455ebe3bc7b5b0b8081e2e38787af25ed6aa
goldragoon <paganinist@gmail.com>
2014-01-30 09:21:04 +0900

    Before REInstall

--- 01.Kernel32/Source/Page.c
+++ 01.Kernel32/Source/Page.c
@@ -1,5 +1,19 @@
 #include "Page.h"
 
-void kInitializePageTable(void);
+void kInitializePageTable(void)ccc
+{
+	DWORD *counter = 0x600000;
+	
+	while(*counter != 0x00)
+	{
+		*counter
+		
+	}
+}
+
 void kSetPageEntryData(PTENTRY* pstEntry, DWORD dwUpperBaseAddress, DWORD dwLowerBaseAddress, 
-        DWORD dwLowerFlags, DWORD dwUpperFlags);
+        DWORD dwLowerFlags, DWORD dwUpperFlags)
+        {
+			
+			
+		}
